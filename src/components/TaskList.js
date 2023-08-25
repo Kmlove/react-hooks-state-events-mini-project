f7b35a2cd9202f17066f87c54b71b77e0ff97405
Kim Love <kimberlymlove15@gmail.com>
2023-08-25 15:03:05 -0400

    Task list populates on page load

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({ tasks }) {
+
+  const taskListArray = tasks.map(task => {
+    return <Task key={task.text} category={task.category} text={task.text}/>
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskListArray}
     </div>
   );
 }
